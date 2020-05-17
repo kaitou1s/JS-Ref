51b1b4f4e6ca36a258410cf48fab6200bca0e990
HoangVv-uet <hoangk62uet@gmail.com>
2020-05-17 16:14:08 +0700

    add somen news

---
+++ Es6news.js
@@ -0,0 +1,13 @@
+// Let and const
+
+// Blocks and IIFE( immediately invoked function expression)
+{
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+(function() {
+    const x = 2;
+})();
+console.log(x);
