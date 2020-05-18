417451bbeb199b67aa0c9f8dbaa40a7153de2159
HoangVv-uet <hoangk62uet@gmail.com>
2020-05-18 10:48:23 +0700

    add some news

--- Es6news.js
+++ Es6news.js
@@ -10,4 +10,69 @@
 (function() {
     const x = 2;
 })();
-console.log(x);
+// console.log(x);
+// Strings
+// ES5
+var firstName = 'Vu';
+var lastName = 'Hoang';
+var n = firstName + ' ' + lastName;
+console.log(n);
+// ES6
+var name = `${firstName} ${lastName}`;
+console.log(name);
+//Lecture: Destructuring
+
+// Array
+var animal = ['dog', 'cat'];
+// ES5
+var d = animal[0];
+var c = animal[1];
+// ES6
+var [d, c] = animal;
+console.log(d);
+console.log(c);
+//Object
+var obj = {
+    firstName: 'Vu',
+    lastName: 'Hoang'
+};
+var {
+    firstName,
+    lastName
+} = obj;
+console.log(firstName);
+console.log(lastName);
+/*
+var {
+    firstName: a,
+    lastName: b
+} = obj;
+console.log(a);
+console.log(b);
+ */
+//Function
+function calcRetirement(yearOfBirth) {
+    var age = new Date().getFullYear() - yearOfBirth;
+    return [age, 65 - age];
+}
+var [age1, retirement] = calcRetirement(1999);
+console.log(age1);
+console.log(retirement);
+// Map, reduce and filter
+var arr = [1, 2, 3, 4, 5, 6];
+var arr1 = arr.map(function(currentValue, index) {
+    return currentValue * 2;
+})
+console.log(arr1);
+var arr2 = arr.filter(function(currentValue, index) {
+    return currentValue > 3;
+})
+console.log(arr2);
+var sum = arr.reduce(function(accumulator, currentValue, index) {
+    return accumulator + currentValue;
+}, 0)
+console.log(sum);
+// Map()
+var question = new Map();
+question.set('title', 'Hoang co may nguoi yeu roi?');
+console.log(question.get('title'));
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# fvdfvfd
+map, filter, reduce in array // Es6news.js
+Syntax:
+map:
+arr.map(function(currentValue, index, arr), thisValue)
+-- function:
+            -currentValue : Required. Value of current element.
+            -index: Optional. The array index of current element.
+
+arr.filter(function(currentValue, index, arr), thisValue)
+-- function:
+            -currentValue : Required. Value of current element.
+            -index: Optional. The array index of current element.
+arr.reduce(function(accmulator, currentValue, index, arr), initialValue)
+            -accmulator: Required. The accmulator accmulates callback's return valuess. It is the accmulated value previously returned in the last invocation of the callback
+            -currentValue: Required. Value of current element.
+            -index: Optional. The array index of current element.
